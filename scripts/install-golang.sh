6dec627e02d503a86100310d3bae094b7854eaf4
Sam Sepiol <38671028+e-corp-sam-sepiol@users.noreply.github.com>
2018-10-09 23:09:38 -0400

    Update install-golang.sh

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -1,60 +1,161 @@
 #!/bin/bash
-# bash script to install golang 1.10.3
+# Golang-Install
+# Project Home Page:
+# https://github.com/skiy/golang-install
+# Author: Skiychan <dev@skiy.net>
+# Link: https://www.skiy.net
+# Modified: Sam Sepiol
 
-# install depends for detection; check for lshw, install if not
-if [ $(dpkg-query -W -f='${Status}' lshw 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    echo "Installing required dependencies to run install-golang..."    
-    apt-get install lshw -y
-fi
+set -e
 
-# install depends for detection; check for git, install if not
-if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    echo "Installing required dependencies to run install-golang..."    
-    apt-get install git -y
-fi
+RELEASES_URL="https://golang.google.cn/dl/"
 
-user=$(logname)
-userhome='/home/'$user
-SYSTEM="$(lshw -short | grep system | awk -F'[: ]+' '{print $3" "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11}' | awk '{print $1}')"
-
-# download and install new version of golang
-function install_go { 
-    # go home first
-    cd "$userhome"/
-    while true; do
-        # check if system is a raspberry pi, grep for only inet if true, print the 2nd column
-        if [[ $SYSTEM = "Raspberry" ]]; then
-            # grab armhf arch for raspberry pi  
-            curl -L -O https://dl.google.com/go/go1.10.3.linux-armv6l.tar.gz
-            tar -C /usr/local -xzf go1.10.3.linux-armv6l.tar.gz
-            break 
-        elif [[ $SYSTEM = "Rockchip" ]]; then
-            # grab arm64 arch for rock64 
-            curl -L -O https://dl.google.com/go/go1.10.3.linux-arm64.tar.gz
-            tar -C /usr/local -xzf go1.10.3.linux-arm64.tar.gz
-            break
-        else
-                if [[ $KERNEL = "orangepione" ]]; then  
-                    curl -L -O https://dl.google.com/go/go1.10.3.linux-armv6l.tar.gz
-                    tar -C /usr/local -xzf go1.10.3.linux-armv6l.tar.gz
-                else
-                    # grab amd64 arch
-                    curl -L -O https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
-                    tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
-                fi
-            # do nothing        
-            :
-            break
-        fi
-    done
-    # echo enviornment variables to .bashrc which is loaded on each new shell
-    echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/"$user"/.bashrc
-    sudo -u "$user" mkdir -p /home/$user/go
-    echo 'export GOPATH=$HOME/go' >> /home/"$user"/.bashrc
-    # export environment variables to current shell     
-    export GOPATH=$HOME/go    
-    export PATH=$PATH:/usr/local/go/bin
-    # display go version
-    go version
+# Get OS bit
+initArch() {
+    ARCH=$(uname -m)
+    BIT=$ARCH
+    case $ARCH in
+        amd64) ARCH="amd64";;
+        x86_64) ARCH="amd64";;
+        i386) ARCH="386";;
+        armv6l) ARCH="armv6l";; 
+        armv7l) ARCH="armv6l";; 
+        *) echo -e "\033[1;31mArchitecture ${ARCH} is not supported by this installation script\033[0m"; exit 1;;
+    esac
+    echo "ARCH = $ARCH"
 }
-install_go
+
+# Get OS version
+initOS() {
+    OS=$(uname | tr '[:upper:]' '[:lower:]')
+    case "$OS" in
+        darwin) OS='darwin';;
+        linux) OS='linux';;
+        freebsd) OS='freebsd';;
+#        mingw*) OS='windows';;
+#        msys*) OS='windows';;
+        *) echo -e "\033[1;31mOS ${OS} is not supported by this installation script\033[0m"; exit 1;;
+    esac
+    echo "OS = $OS"
+}
+
+# Compare Version
+compareVersion() {
+    OLD_VERSION="none"
+    NEW_VERSION="$1"
+    if test -x "$(command -v go)"; then
+        OLD_VERSION="$(go version | awk '{print $3}')"
+    fi
+    if [ "$OLD_VERSION" = "$NEW_VERSION" ]; then
+       echo -e "\033[1;31mYou have installed this version: $OLD_VERSION\033[0m"; exit 1;
+    fi
+
+printf "
+Current version: \033[1;33m$OLD_VERSION\033[0m 
+Target version: \033[1;33m$NEW_VERSION\033[0m
+"
+}
+
+# Check if user is root
+checkRoot() {
+    ROOT=$(id -u)
+    case "$ROOT" in
+        0) ROOT='root';;
+        *) echo -e "\033[1;31mError: You must be root to run this script\033[0m"; exit 1;;
+    esac
+}
+
+# Download go file
+downloadFile() {
+    url="$1"
+    destination="$2"
+
+    echo -e "Fetching $url.. \n"
+    if test -x "$(command -v curl)"; then
+        code=$(curl -s -w '%{http_code}' -L "$url" -o "$destination")
+    elif test -x "$(command -v wget)"; then
+        code=$(wget -q -O "$destination" --server-response "$url" 2>&1 | awk '/^  HTTP/{print $2}' | tail -1)
+    else
+        echo "Neither curl nor wget was available to perform http requests."
+        exit 1
+    fi
+
+    if [ "$code" != 200 ]; then
+        echo "Request failed with code $code"
+        exit 1
+    fi
+}
+
+# Set golang environment
+setEnvironment() {
+    profile="$1"
+    if [ -z "`grep 'export\sGOROOT' $profile`" ];then
+        echo "export GOROOT=/usr/local/go" >> $profile
+    fi
+    if [ -z "`grep 'export\sGOPATH' $profile`" ];then
+        echo "export GOPATH=/data/go" >> $profile
+    fi
+    if [ -z "`grep 'export\sGOBIN' $profile`" ];then
+        echo "export GOBIN=/data/go/bin" >> $profile
+    fi   
+    if [ -z "`grep '\$GOROOT/bin:\$GOBIN' $profile`" ];then
+        echo "export PATH=\$GOROOT/bin:\$GOBIN:\$PATH" >> $profile
+    fi  
+}
+
+# Printf version info
+clear
+printf "
+###############################################################
+###  Golang Install
+###  Author Skiychan<dev@skiy.net>
+###  Link https://www.skiy.net 
+###############################################################
+\n"
+
+# identify platform based on uname output
+checkRoot
+initArch
+initOS
+
+# DIY version
+if [ -n "$1" ] ;then
+    RELEASE_TAG="go$1"
+fi
+
+# if RELEASE_TAG was not provided, assume latest
+if [ -z "$RELEASE_TAG" ]; then
+    RELEASE_TAG="$(curl -sL $RELEASES_URL | sed -n '/toggleVisible/p' | head -n 1 | cut -d '"' -f 4)"
+fi
+echo "Release Tag = $RELEASE_TAG"
+
+# Compare version
+compareVersion $RELEASE_TAG
+
+printf "
+###############################################################
+###  System: %s 
+###  Bit: %s 
+###  Version: %s 
+###############################################################
+\n" $OS $BIT $RELEASE_TAG
+
+# Download File
+BINARY_URL="https://dl.google.com/go/$RELEASE_TAG.$OS-$ARCH.tar.gz"
+DOWNLOAD_FILE="$(mktemp).tar.gz"
+downloadFile "$BINARY_URL" "$DOWNLOAD_FILE"
+
+# Tar file and move file
+tar -C /usr/local/ -zxf $DOWNLOAD_FILE && \
+rm  -rf $DOWNLOAD_FILE
+
+# Create GOPATH folder
+sudo -u "$user" mkdir -p /home/$user/go
+# echo enviornment variables to .bashrc which is loaded on each new shell
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/"$user"/.bashrc
+echo 'export GOPATH=$HOME/go' >> /home/"$user"/.bashrc
+# export environment variables to current shell     
+export GOPATH=$HOME/go    
+export PATH=$PATH:/usr/local/go/bin
+# display go version
+go version
